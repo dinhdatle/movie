86cb8ee23274a839fbb31a15d3faf8f29e311442
dinhdatle <111744077+dinhdatle@users.noreply.github.com>
2023-03-12 21:32:50 +0700

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react'
 import {fetchDataFromApi} from "./utils/api"
 import { useSelector, useDispatch } from 'react-redux'
-import { getApiConfigrtation } from './store/homeSlice'
+import { getApiConfigrtation,getGenres } from './store/homeSlice'
 
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
@@ -18,21 +18,36 @@ function App() {
 
   useEffect(()=>{
     fetchApiConfig()
+    genresCall()
   },[])
 
   const fetchApiConfig = () => {
     fetchDataFromApi('/configuration').then((res)=> {
-      console.log(res);
+      // console.log(res);
 
       const url = {
         backdrop: res.images.secure_base_url + "original",
         poster: res.images.secure_base_url + "original",
         profile: res.images.secure_base_url + "original"
       }
-
       dispatch(getApiConfigrtation(url));
-
     })
+  }
+
+  const genresCall =  async () => {
+    let promises = []
+    let endPoints = ["tv","movie"]
+    let allGenres = {}
+
+    endPoints.forEach((url) => {
+       promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    });
+
+    const data = await Promise.all(promises)
+    data.map(({genres})=>{
+      return genres.map((item) => (allGenres[item.id] = item))
+    })
+    dispatch(getGenres(allGenres))
   }
   
   return (
